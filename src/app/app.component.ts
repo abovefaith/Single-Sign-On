eab87a2f8b094196f5553d28f7cf7d61107645e7
Faith Kawira <faith.kawira@kamsware.com>
2025-07-18 17:46:23 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,63 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { authCodeFlowConfig } from './sso.config';
+import { JwksValidationHandler } from 'angular-oauth2-oidc-jwks';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'sso-app';
+
+  private oauthService = inject(OAuthService);
+
+  constructor() {
+    this.configureSingleSignOn();
+  }
+
+  configureSingleSignOn() {
+    this.oauthService.configure(authCodeFlowConfig);
+    this.oauthService.tokenValidationHandler = new JwksValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    // Logic for login
+    this.oauthService.initImplicitFlow();
+    console.log('Login button clicked');
+  }
+
+  logout() {
+    // Logic for logout
+    this.oauthService.logOut();
+    console.log('Logout button clicked');
+  }
+
+  get token() {
+    let claims: any = this.oauthService.getIdentityClaims();
+    return claims ? claims : null;
+  }
+
+  // get token(){
+  //   let claims: any = this.oauthService.getIdentityClaims();
+  //   console.log(claims);
+  //   return claims ? JSON.stringify(claims) : null;
+  // }
+
+
+  // get token(){
+  //   if (!this.oauthService.hasValidAccessToken()) {
+  //     console.log('User is not authenticated');
+  //     return null;
+  //   }
+  //   let claims: any = this.oauthService.getIdentityClaims();
+  //   console.log(claims);
+  //   return claims ? JSON.stringify(claims) : null;
+  // }
 }
